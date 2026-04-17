012456249245c8f0babec0a5bf940860f92a83fb
zacube <contact@zacube.com>
2026-04-17 09:14:43 +0200

    page index (route + twig) page product (route + twig) Correction css (ajout padding sur les .card)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,25 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProductController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'app_index')]
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'products' => $productRepository->findAll(),
         ]);
     }
 
-    #[Route('/product', name: 'app_product')]
-    public function product(): Response
+    #[Route('/product/{id}', name: 'app_product', requirements: ['id' => '\d+'])]
+    public function product(?Product $product): Response
     {
+        // Produit introuvable → redirection
+        if ($product === null) {
+            $this->addFlash('erreur', "Produit inexistant");
+            return $this->redirectToRoute('app_index');
+        }
         return $this->render('product/product.html.twig', [
-            'controller_name' => 'ProductController',
+            'product' => $product,
         ]);
     }
 }
