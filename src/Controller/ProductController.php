50e4cef36f3cbbe316536fd5455c1101ef953163
zacube <contact@zacube.com>
2026-04-22 09:18:57 +0200

    Mise en place de la page Panier

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Basket;
 use App\Entity\Product;
+use App\Repository\BasketRepository;
 use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -29,5 +33,39 @@ final class ProductController extends AbstractController
         return $this->render('product/product.html.twig', [
             'product' => $product,
         ]);
+    }
+
+    #[Route('/product/{id}/add', name: 'app_add_product', requirements: ['id' => '\d+'])]
+    public function add(EntityManagerInterface $em, BasketRepository $basketRepository, ?Product $product): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+        // Produit introuvable → redirection
+        if ($product === null) {
+            $this->addFlash('erreur', "Produit inexistant");
+            return $this->redirectToRoute('app_index');
+        }
+        $basketItem = new Basket()
+            ->setUser($this->getUser())
+            ->setProduct($product)
+            ->setQuantity(1);
+
+        $em->persist($basketItem);
+        $em->flush();
+
+        $basket = $basketRepository->findBy(['user' => ($this->getUser())]);
+        dump($basket);
+
+        $totalPrice = 0;
+        foreach ($basket as $item) {
+            $totalPrice += $item->getProduct()->getPrice();
+        }
+
+
+        return $this->render('user/basket.html.twig', [
+            /*'product' => $product,*/
+            'basket' => $basket,
+            'totalPrice' => $totalPrice
+        ]);
+
     }
 }
